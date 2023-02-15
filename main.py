62ca85425edd429b3b2dcd11afbaedadec208312
User <user@gmail.com>
2023-02-15 09:34:56 +0300

    Галерея с загрузкой

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import base64
-from random import randint, choice
-
+import os
+from random import randint
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+def get_images():
+    lst_images = []
+    for currentdir, dirs, files in os.walk('static/img'):
+        filepath = currentdir.split('/')
+        for file in files:
+            lst_images.append(os.path.join(*filepath, file))
+    return lst_images
+
+
+lst_images = get_images()
 
 
 @app.route('/index')
@@ -12,7 +22,7 @@ def index():
 
 @app.route('/')
 def main():
-    return "Миссия Колонизация Марса"
+    return "Миссия Колони5зация Марса"
 
 @app.route('/promotion')
 def promotion():
@@ -64,14 +74,25 @@ def load_photo():
     if request.method == 'GET':
         return render_template('load_photo.html', photo='')
     elif request.method == 'POST':
-        f = request.files['file']
-        return render_template('load_photo.html', photo=base64.b64encode(f.read()))
+        f = request.files['photo']
+        with open('static/img/photo', 'wb') as ph:
+            ph.write(f.read())
+        return render_template('load_photo.html', photo='static/img/photo')
 
 
-@app.route('/carousel')
+@app.route('/carousel', methods=['GET', 'POST'])
 def carousel():
-    return render_template('carousel.html')
+    global lst_images
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = f'mars{len(lst_images) + 1}'
+        filepath = os.path.join('static', 'img', filename)
+        with open(filepath, 'wb') as ph:
+            ph.write(f.read())
+        lst_images.append(filepath)
+    return render_template('carousel.html', data=lst_images)
+
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1', debug=True)
